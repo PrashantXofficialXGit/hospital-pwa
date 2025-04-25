4a0edc0198274ba29ac0b41171a25b06d979c93c
PrashantXofficialXGit <pkch92277@gmail.com>
2025-04-25 10:28:32 +0530

    Update wsgi.py

--- map_navigation_project/wsgi.py
+++ map_navigation_project/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "map_navigation_project.settings")
 
 application = get_wsgi_application()
+
+app = application
