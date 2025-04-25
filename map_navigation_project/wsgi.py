2de80c764956f1242a66431d3b15fdb3982f18eb
PrashantXofficialXGit <pkch92277@gmail.com>
2025-04-25 17:26:32 +0530

    Update wsgi.py

--- map_navigation_project/wsgi.py
+++ map_navigation_project/wsgi.py
@@ -8,16 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-# Make sure your project base directory is in the path
-path = '/home/PrashantXKumar/hospital-pwa'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'map_navigation_project.settings')
-
-# Load WSGI application
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "map_navigation_project.settings")
+
 application = get_wsgi_application()
